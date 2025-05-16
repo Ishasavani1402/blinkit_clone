e8d29708f293c224143cbf47779d3e59a2c9e24a
Ishasavani1402 <savaniisha57@gmail.com>
2025-05-16 13:18:11 +0530

    save changes

---
+++ lib/list_of_dynamics/grocery.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: non_constant_identifier_names
+
+class Grocery {
+  static List<dynamic> grocery_iteams() {
+    return [
+      {
+        'img':"assets/groceryimages/img1.png",
+        'name':"vegetables and \n fruites"
+      },
+      {
+        'img':"assets/groceryimages/img2.png",
+        'name':"Atta dal and \n rice"
+      },
+      {
+        'img':"assets/groceryimages/img3.png",
+        'name':"oil ghee and \n masala"
+      },
+      {
+        'img':"assets/groceryimages/img4.png",
+        'name':"dairy bread and \n milk"
+      },
+      {
+        'img':"assets/groceryimages/img5.png",
+        'name':"biscutes and \n backery"
+      },
+    ];
+  }
+}
--- lib/screens/bottomnavbar/category.dart
+++ lib/screens/bottomnavbar/category.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api
 
+import 'package:blinkit_clone/widgets/appbar.dart';
+import 'package:blinkit_clone/widgets/custome_text.dart';
 import 'package:flutter/material.dart';
 
 class Category extends StatefulWidget {
@@ -10,9 +12,33 @@ class Category extends StatefulWidget {
 class _CategoryState extends State<Category> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("Category"),
+    return Scaffold(
+      appBar: Custome_Appbar(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.5),
+            child: CustomeText(
+              text: "Grocery and kitchen",
+              fsize: 18,
+              fcolor: Colors.black,
+              family: "bold",
+            ),
+          ),
+          SizedBox(height: 20,),
+          ListView.builder(itemBuilder: (context ,index){
+            return Container(
+              height: 70,
+              width: 70,
+              decoration: BoxDecoration(
+                color: Colors.blue.shade100,
+                borderRadius: BorderRadius.circular(10)
+              ),
+              
+            );
+
+          })
+        ],
       ),
     );
   }
