5920dd2246e3373c1dcf00f40047067c360da1f2
Ishasavani1402 <savaniisha57@gmail.com>
2025-05-16 14:58:15 +0530

    save changes

--- lib/list_of_dynamics/grocery.dart
+++ lib/list_of_dynamics/grocery.dart
@@ -4,24 +4,21 @@ class Grocery {
   static List<dynamic> grocery_iteams() {
     return [
       {
-        'img':"assets/groceryimages/img1.png",
-        'name':"vegetables and \n fruites"
+        'img': "assets/groceryimages/img1.png",
+        'name': "vegetables and \n fruites",
+      },
+      {'img': "assets/groceryimages/img2.png", 'name': "Atta dal and \n rice"},
+      {
+        'img': "assets/groceryimages/img3.png",
+        'name': "oil ghee and \n masala",
       },
       {
-        'img':"assets/groceryimages/img2.png",
-        'name':"Atta dal and \n rice"
+        'img': "assets/groceryimages/img4.png",
+        'name': "dairy bread and \n milk",
       },
       {
-        'img':"assets/groceryimages/img3.png",
-        'name':"oil ghee and \n masala"
-      },
-      {
-        'img':"assets/groceryimages/img4.png",
-        'name':"dairy bread and \n milk"
-      },
-      {
-        'img':"assets/groceryimages/img5.png",
-        'name':"biscutes and \n backery"
+        'img': "assets/groceryimages/img5.png",
+        'name': "biscutes and \n backery",
       },
     ];
   }
--- lib/screens/bottomnavbar/category.dart
+++ lib/screens/bottomnavbar/category.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api
 
+import 'package:blinkit_clone/list_of_dynamics/grocery.dart';
 import 'package:blinkit_clone/widgets/appbar.dart';
 import 'package:blinkit_clone/widgets/custome_text.dart';
 import 'package:flutter/material.dart';
@@ -10,11 +11,13 @@ class Category extends StatefulWidget {
 }
 
 class _CategoryState extends State<Category> {
+  var grocary = Grocery.grocery_iteams();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Custome_Appbar(),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.all(10.5),
@@ -25,21 +28,45 @@ class _CategoryState extends State<Category> {
               family: "bold",
             ),
           ),
-          SizedBox(height: 20,),
-          ListView.builder(itemBuilder: (context ,index){
-            return Container(
-              height: 70,
-              width: 70,
-              decoration: BoxDecoration(
-                color: Colors.blue.shade100,
-                borderRadius: BorderRadius.circular(10)
-              ),
-              
-            );
-
-          })
+          SizedBox(
+            height: 200,
+            child: ListView.builder(
+              itemCount: grocary.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          color: Colors.blue.shade100,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Image.asset(
+                            grocary[index]['img'].toString(),
+                          ),
+                        ),
+                      ),
+                    ),
+                    CustomeText(
+                      text: grocary[index]['name'].toString(),
+                      fsize: 12,
+                      fcolor: Colors.black,
+                      family: "regular",
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
+
